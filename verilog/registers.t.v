6a42f8ecac0e69e5ba6997d9bc3a371a15864bb4
ccellis <coleman.ellis@students.olin.edu>
2018-12-10 09:54:56 -0500

    Working XBus testbench, XBus bugfixes

--- verilog/registers.t.v
+++ verilog/registers.t.v
@@ -9,8 +9,15 @@ module testRegs();
 	reg[10:0] p1_in = 11'b0;
 	reg[2:0] read_addr0 = 3'b0;
 	reg[2:0] read_addr1 = 3'b0;
+	reg[10:0] x0_in = 11'b0;
+	reg[10:0] x1_in = 11'b0;
+	reg x0_write_in = 1'b0;
+	reg x1_write_in = 1'b0;
+	reg x0_read_in = 1'b0;
+	reg x1_read_in = 1'b0;
 
-	wire[10:0] p0_out, p1_out, dat_out0, dat_out1;
+	wire[10:0] p0_out, p1_out, dat_out0, dat_out1, x0_out, x1_out;
+	wire x0_write_out, x1_write_out, x0_read_out, x1_read_out, increment_pc;
 
 	MC3999regFile dut(
 		.write_dat(write_dat),
@@ -20,11 +27,25 @@ module testRegs();
 		.p1_in(p1_in),
 		.read_addr0(read_addr0),
 		.read_addr1(read_addr1),
+		.x0_in(x0_in),
+		.x1_in(x1_in),
+		.x0_read_in(x0_read_in),
+		.x1_read_in(x1_read_in),
+		.x0_write_in(x0_write_in),
+		.x1_write_in(x1_write_in),
 		.clk(clk),
+
 		.p0_out(p0_out),
 		.p1_out(p1_out),
 		.dat_out0(dat_out0),
-		.dat_out1(dat_out1));
+		.dat_out1(dat_out1),
+		.x0_out(x0_out),
+		.x1_out(x1_out),
+		.x0_read_out(x0_read_out),
+		.x1_read_out(x1_read_out),
+		.x0_write_out(x0_write_out),
+		.x1_write_out(x1_write_out),
+		.increment_pc(increment_pc));
 
 	always #10 clk = !clk;
 
@@ -73,6 +94,65 @@ module testRegs();
 		#20
 		if(dat_out1 != 79 || dat_out0 != 79)
 			$display("Write not disabled by write_en");
+
+		#20
+		write_en = 1;
+		write_dat = 11'd444;
+		write_addr = 3'b100; //x0
+		x0_read_in = 0;
+		#20
+		if(x0_write_out != 1)
+			$display("XBus write not raising flag");
+		if(x0_out != 11'd444)
+			$display("XBus write not outputting value");
+		if(increment_pc == 1)
+			$display("XBus write not halting PC");
+
+		#40
+		if(increment_pc == 1 || x0_write_out != 1 || x0_out != 11'd444)
+			$display("XBus write doesn't hold state across cycles");
+		#20
+		x0_read_in = 1;
+		#10
+		x0_read_in = 0;
+		write_en = 0;
+		write_dat = 0;
+		write_addr = 0;
+		#10
+		if (increment_pc == 0)
+			$display("XBus write doesn't increment PC when read_in goes high");
+		if (x0_write_out != 0)
+			$display("XBus write flag doesn't go low after writing");
+		if(x0_out != 0)
+			$display("XBus output doesn't clear after writing");
+
+		#20
+		read_addr0 = 3'b101;
+		x1_write_in = 0;
+		#20
+		if (increment_pc == 1)
+			$display("XBus read doesn't halt PC");
+		if (x1_read_out != 1)
+			$display("XBus read doesn't rais read flag");
+
+		#40
+		if(increment_pc == 1 || x1_read_out != 1)
+			$display("XBus read doesn't hold state across cycles");
+		#20
+		x1_write_in = 1;
+		x1_in = 11'd777;
+		#5
+		if (dat_out0 != 11'd777)
+			$display("XBus read not reading value when written to.");
+		#5
+		read_addr0 = 0;
+		x1_write_in = 0;
+		x1_in = 0;
+		#10
+		if(x0_read_out == 1)
+			$display("XBus read not clearing flag after read");
+
+		
 
 		#40
 
--- verilog/registers.v
+++ verilog/registers.v
@@ -30,6 +30,13 @@ reg[10:0] acc = 11'b0;
 initial begin
 	p0_out = 11'b0;
 	p1_out = 11'b0;
+	x0_out = 11'b0;
+	x1_out = 11'b0;
+	x0_read_out = 1'b0;
+	x1_read_out = 1'b0;
+	x0_write_out = 1'b0;
+	x1_write_out = 1'b0;
+	increment_pc = 1'b0;
 end
 
 always @(posedge clk) begin
@@ -45,16 +52,8 @@ always @(posedge clk) begin
 					x0_write_out = 1;
 					x0_out = write_dat;
 				end
-				else begin
-					// If you're writing and HAVE pushed data
-					// If you're being read, clear outputs
-					if (x0_read_in == 1) begin
-						x0_out = 0;
-						x0_write_out = 0;
-					end // if (x0_read_in == 1)
-					//If you're not being read, don't change anything about your outputs
-					//The PC won't increment
-				end
+				//Once the write_out flag is high, PC won't increment until the 
+				//Input read flag goes high
 			end // x0
 
 			3'b101: begin  //x1
@@ -62,27 +61,30 @@ always @(posedge clk) begin
 					x1_write_out = 1;
 					x1_out = write_dat;
 				end // if (x1_write_out == 0)
-				else begin
-					if (x1_read_in == 1) begin
-						x1_out = 0;
-						x1_write_out = 0;
-					end // if (x1_read_in == 1)
-				end
 			end // x0
 		endcase // write_addr
+	end // if (write_en)
 
-		// If a simple input was read, clear its output
+	// If not writing to an xbus, clear their outputs
+	if (write_addr != 3'b100) begin
+		x0_write_out = 0;
+		x0_out = 0;
+	end
+	if (write_addr != 3'b101) begin
+		x1_write_out = 0;
+		x1_out = 0;
+	end
+
+	// If a simple input was read, clear its output
 		case (read_addr0)
-			3'b010: p0_out = 3'b0;
-			3'b011: p1_out = 3'b0;
+			3'b010: p0_out = 11'b0;
+			3'b011: p1_out = 11'b0;
 		endcase
 
 		case (read_addr1)
-			3'b010: p0_out = 3'b0;
-			3'b011: p1_out = 3'b0;
+			3'b010: p0_out = 11'b0;
+			3'b011: p1_out = 11'b0;
 		endcase
-
-	end // if (write_en)
 end
 
 always @(*) begin
@@ -91,8 +93,8 @@ always @(*) begin
 		3'b010: begin dat_out0 = p0_in; end //When you read a simple input
 		3'b011: begin dat_out0 = p1_in; end //Clear the corresponding output
 
-		3'b100:	x0_read_out = 1; //x0
-		3'b101: x1_read_out = 1; //x1
+		3'b100:	begin x0_read_out = 1; dat_out0 = x0_in; end //x0
+		3'b101: begin x1_read_out = 1; dat_out0 = x1_in; end //x1
 		default: dat_out0 = 11'b0;
 	endcase
 
@@ -101,8 +103,8 @@ always @(*) begin
 		3'b010: begin dat_out1 = p0_in; end
 		3'b011: begin dat_out1 = p1_in; end
 
-		3'b100:	x0_read_out = 1; //x0
-		3'b101: x1_read_out = 1; //x1
+		3'b100:	begin x0_read_out = 1; dat_out1 = x0_in; end //x0
+		3'b101: begin x1_read_out = 1; dat_out1 = x1_in; end //x1
 		default: dat_out1 = 11'b0;
 	endcase
 
@@ -115,7 +117,7 @@ always @(*) begin
 	//if we're reading or writing an xbus
 	if (write_addr == 3'b100 || write_addr == 3'b101 || read_addr0 == 3'b100 || read_addr0 == 3'b101 || read_addr1 == 3'b100 || read_addr1 == 3'b101) begin
 		// If you're writing, but haven't pushed the output yet, don't increment pc
-		if (x0_write_out == 0 || x1_write_out == 0)
+		if ((x0_write_out == 0 && write_addr == 3'b100) || (x1_write_out == 0 && write_addr == 3'b101))
 			increment_pc = 0;
 		// If you're writing and HAVE pushed the output, but it's not been read yet, don't increment pc
 		else if ((x0_write_out == 1 && x0_read_in == 0) || (x1_write_out == 1 && x0_read_in == 0))
@@ -126,6 +128,9 @@ always @(*) begin
 		else
 			increment_pc = 1;
 	end
+	//If no xbus things, increment pc
+	else
+		increment_pc = 1;
 
 end
 
